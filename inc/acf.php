906342db61b5289de3452fcd137ec02787fea919
Amri Karisma <amrikarisma@yahoo.com>
2021-08-05 00:22:28 +0700

    update new

--- inc/acf.php
+++ inc/acf.php
@@ -1,25 +1,31 @@
 <?php
 
 // Define path and URL to the ACF plugin.
-define( 'MY_ACF_PATH', get_template_directory() . '/inc/acf/' );
-define( 'MY_ACF_URL', get_template_directory_uri() . '/inc/acf/' );
+define('MY_ACF_PATH', get_template_directory() . '/inc/acf/');
+define('MY_ACF_URL', get_template_directory_uri() . '/inc/acf/');
 
 // Include the ACF plugin.
-include_once( MY_ACF_PATH . 'acf.php' );
+include_once(MY_ACF_PATH . 'acf.php');
 
 // Customize the url setting to fix incorrect asset URLs.
 add_filter('acf/settings/url', 'my_acf_settings_url');
-function my_acf_settings_url( $url ) {
+function my_acf_settings_url($url)
+{
     return MY_ACF_URL;
 }
 
 // (Optional) Hide the ACF admin menu item.
 add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
-function my_acf_settings_show_admin( $show_admin ) {
-    return false;
+function my_acf_settings_show_admin($show_admin)
+{
+    if (WP_DEBUG === true) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-if( function_exists('acf_add_local_field_group') ):
+if (function_exists('acf_add_local_field_group')) :
 
     acf_add_local_field_group(array(
         'key' => 'group_5f90239a3b8ab',
@@ -316,5 +322,5 @@ if( function_exists('acf_add_local_field_group') ):
         'active' => true,
         'description' => '',
     ));
-    
-    endif;
+
+endif;
