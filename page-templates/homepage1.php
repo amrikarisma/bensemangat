f670cf9cc6ebd76935773e02d030ee4549fffd94
Amri Karisma <amrikarisma@yahoo.com>
2021-08-08 04:52:04 +0700

    update

--- page-templates/homepage1.php
+++ page-templates/homepage1.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Template Name: Homepage Template
+ * Template Name: Homepage1 Template
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
@@ -9,12 +10,12 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 
-	get_template_part( 'component-templates/comp', 'slider' );
-	get_template_part( 'component-templates/comp', 'featured' );
-	get_template_part( 'component-templates/comp', 'product' );
+get_template_part('component-templates/comp', 'slider');
+get_template_part('component-templates/comp', 'featured');
+get_template_part('component-templates/comp', 'product');
 
 get_footer();
