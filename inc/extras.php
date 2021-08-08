b888a6f604ee3aa7a9ee81b324b2647f09f5f2c5
Amri Karisma <amrikarisma@yahoo.com>
2021-08-08 14:02:51 +0700

    add customize phone email and social media

--- inc/extras.php
+++ inc/extras.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Custom functions that act independently of the theme templates
  *
@@ -8,11 +9,11 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-add_filter( 'body_class', 'bensemangat_body_classes' );
+add_filter('body_class', 'bensemangat_body_classes');
 
-if ( ! function_exists( 'bensemangat_body_classes' ) ) {
+if (!function_exists('bensemangat_body_classes')) {
 	/**
 	 * Adds custom classes to the array of body classes.
 	 *
@@ -20,13 +21,14 @@ if ( ! function_exists( 'bensemangat_body_classes' ) ) {
 	 *
 	 * @return array
 	 */
-	function bensemangat_body_classes( $classes ) {
+	function bensemangat_body_classes($classes)
+	{
 		// Adds a class of group-blog to blogs with more than 1 published author.
-		if ( is_multi_author() ) {
+		if (is_multi_author()) {
 			$classes[] = 'group-blog';
 		}
 		// Adds a class of hfeed to non-singular pages.
-		if ( ! is_singular() ) {
+		if (!is_singular()) {
 			$classes[] = 'hfeed';
 		}
 
@@ -34,18 +36,18 @@ if ( ! function_exists( 'bensemangat_body_classes' ) ) {
 	}
 }
 
-if ( function_exists( 'bensemangat_adjust_body_class' ) ) {
+if (function_exists('bensemangat_adjust_body_class')) {
 	/*
 	 * bensemangat_adjust_body_class() deprecated in v0.9.4. We keep adding the
 	 * filter for child themes which use their own bensemangat_adjust_body_class.
 	 */
-	add_filter( 'body_class', 'bensemangat_adjust_body_class' );
+	add_filter('body_class', 'bensemangat_adjust_body_class');
 }
 
 // Filter custom logo with correct classes.
-add_filter( 'get_custom_logo', 'bensemangat_change_logo_class' );
+add_filter('get_custom_logo', 'bensemangat_change_logo_class');
 
-if ( ! function_exists( 'bensemangat_change_logo_class' ) ) {
+if (!function_exists('bensemangat_change_logo_class')) {
 	/**
 	 * Replaces logo CSS class.
 	 *
@@ -53,99 +55,135 @@ if ( ! function_exists( 'bensemangat_change_logo_class' ) ) {
 	 *
 	 * @return string
 	 */
-	function bensemangat_change_logo_class( $html ) {
+	function bensemangat_change_logo_class($html)
+	{
 
-		$html = str_replace( 'class="custom-logo"', 'class="img-fluid"', $html );
-		$html = str_replace( 'class="custom-logo-link"', 'class="navbar-brand custom-logo-link"', $html );
-		$html = str_replace( 'alt=""', 'title="Home" alt="logo"', $html );
+		$html = str_replace('class="custom-logo"', 'class="img-fluid"', $html);
+		$html = str_replace('class="custom-logo-link"', 'class="navbar-brand custom-logo-link"', $html);
+		$html = str_replace('alt=""', 'title="Home" alt="logo"', $html);
 
 		return $html;
 	}
 }
 
 
-if ( ! function_exists( 'bensemangat_post_nav' ) ) {
+if (!function_exists('bensemangat_post_nav')) {
 	/**
 	 * Display navigation to next/previous post when applicable.
 	 */
-	function bensemangat_post_nav() {
+	function bensemangat_post_nav()
+	{
 		// Don't print empty markup if there's nowhere to navigate.
-		$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-		$next     = get_adjacent_post( false, '', false );
+		$previous = (is_attachment()) ? get_post(get_post()->post_parent) : get_adjacent_post(false, '', true);
+		$next     = get_adjacent_post(false, '', false);
 
-		if ( ! $next && ! $previous ) {
+		if (!$next && !$previous) {
 			return;
 		}
-		?>
+?>
 		<nav class="container navigation post-navigation">
-			<h2 class="sr-only"><?php esc_html_e( 'Post navigation', 'bensemangat' ); ?></h2>
+			<h2 class="sr-only"><?php esc_html_e('Post navigation', 'bensemangat'); ?></h2>
 			<div class="row nav-links justify-content-between">
 				<?php
-				if ( get_previous_post_link() ) {
-					previous_post_link( '<span class="nav-previous">%link</span>', _x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'bensemangat' ) );
+				if (get_previous_post_link()) {
+					previous_post_link('<span class="nav-previous">%link</span>', _x('<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'bensemangat'));
 				}
-				if ( get_next_post_link() ) {
-					next_post_link( '<span class="nav-next">%link</span>', _x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'bensemangat' ) );
+				if (get_next_post_link()) {
+					next_post_link('<span class="nav-next">%link</span>', _x('%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'bensemangat'));
 				}
 				?>
 			</div><!-- .nav-links -->
 		</nav><!-- .navigation -->
-		<?php
+	<?php
+	}
+}
+if (!function_exists('bensemangat_post_nav_custom')) {
+	/**
+	 * Display navigation to next/previous post when applicable.
+	 */
+	function bensemangat_post_nav_custom()
+	{
+		// Don't print empty markup if there's nowhere to navigate.
+		$previous = (is_attachment()) ? get_post(get_post()->post_parent) : get_adjacent_post(false, '', true);
+		$next     = get_adjacent_post(false, '', false);
+
+		if (!$next && !$previous) {
+			return;
+		}
+	?>
+		<nav class="container navigation post-navigation">
+			<h2 class="sr-only"><?php esc_html_e('Post navigation', 'bensemangat'); ?></h2>
+			<div class="row nav-links justify-content-between">
+				<?php
+				if (get_previous_post_link()) {
+					previous_post_link('<span class="nav-previous">%link</span>', _x('<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'bensemangat'));
+				}
+				if (get_next_post_link()) {
+					next_post_link('<span class="nav-next">%link</span>', _x('%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'bensemangat'));
+				}
+				?>
+			</div><!-- .nav-links -->
+		</nav><!-- .navigation -->
+<?php
 	}
 }
 
-if ( ! function_exists( 'bensemangat_pingback' ) ) {
+if (!function_exists('bensemangat_pingback')) {
 	/**
 	 * Add a pingback url auto-discovery header for single posts of any post type.
 	 */
-	function bensemangat_pingback() {
-		if ( is_singular() && pings_open() ) {
-			echo '<link rel="pingback" href="' . esc_url( get_bloginfo( 'pingback_url' ) ) . '">' . "\n";
+	function bensemangat_pingback()
+	{
+		if (is_singular() && pings_open()) {
+			echo '<link rel="pingback" href="' . esc_url(get_bloginfo('pingback_url')) . '">' . "\n";
 		}
 	}
 }
-add_action( 'wp_head', 'bensemangat_pingback' );
+add_action('wp_head', 'bensemangat_pingback');
 
-if ( ! function_exists( 'bensemangat_mobile_web_app_meta' ) ) {
+if (!function_exists('bensemangat_mobile_web_app_meta')) {
 	/**
 	 * Add mobile-web-app meta.
 	 */
-	function bensemangat_mobile_web_app_meta() {
+	function bensemangat_mobile_web_app_meta()
+	{
 		echo '<meta name="mobile-web-app-capable" content="yes">' . "\n";
 		echo '<meta name="apple-mobile-web-app-capable" content="yes">' . "\n";
-		echo '<meta name="apple-mobile-web-app-title" content="' . esc_attr( get_bloginfo( 'name' ) ) . ' - ' . esc_attr( get_bloginfo( 'description' ) ) . '">' . "\n";
+		echo '<meta name="apple-mobile-web-app-title" content="' . esc_attr(get_bloginfo('name')) . ' - ' . esc_attr(get_bloginfo('description')) . '">' . "\n";
 	}
 }
-add_action( 'wp_head', 'bensemangat_mobile_web_app_meta' );
+add_action('wp_head', 'bensemangat_mobile_web_app_meta');
 
-if ( ! function_exists( 'bensemangat_default_body_attributes' ) ) {
+if (!function_exists('bensemangat_default_body_attributes')) {
 	/**
 	 * Adds schema markup to the body element.
 	 *
 	 * @param array $atts An associative array of attributes.
 	 * @return array
 	 */
-	function bensemangat_default_body_attributes( $atts ) {
+	function bensemangat_default_body_attributes($atts)
+	{
 		$atts['itemscope'] = '';
 		$atts['itemtype']  = 'http://schema.org/WebSite';
 		return $atts;
 	}
 }
-add_filter( 'bensemangat_body_attributes', 'bensemangat_default_body_attributes' );
+add_filter('bensemangat_body_attributes', 'bensemangat_default_body_attributes');
 
 // Escapes all occurances of 'the_archive_description'.
-add_filter( 'get_the_archive_description', 'bensemangat_escape_the_archive_description' );
+add_filter('get_the_archive_description', 'bensemangat_escape_the_archive_description');
 
-if ( ! function_exists( 'bensemangat_escape_the_archive_description' ) ) {
+if (!function_exists('bensemangat_escape_the_archive_description')) {
 	/**
 	 * Escapes the description for an author or post type archive.
 	 *
 	 * @param string $description Archive description.
 	 * @return string Maybe escaped $description.
 	 */
-	function bensemangat_escape_the_archive_description( $description ) {
-		if ( is_author() || is_post_type_archive() ) {
-			return wp_kses_post( $description );
+	function bensemangat_escape_the_archive_description($description)
+	{
+		if (is_author() || is_post_type_archive()) {
+			return wp_kses_post($description);
 		}
 
 		/*
@@ -157,19 +195,20 @@ if ( ! function_exists( 'bensemangat_escape_the_archive_description' ) ) {
 } // End of if function_exists( 'bensemangat_escape_the_archive_description' ).
 
 // Escapes all occurances of 'the_title()' and 'get_the_title()'.
-add_filter( 'the_title', 'bensemangat_kses_title' );
+add_filter('the_title', 'bensemangat_kses_title');
 
 // Escapes all occurances of 'the_archive_title' and 'get_the_archive_title()'.
-add_filter( 'get_the_archive_title', 'bensemangat_kses_title' );
+add_filter('get_the_archive_title', 'bensemangat_kses_title');
 
-if ( ! function_exists( 'bensemangat_kses_title' ) ) {
+if (!function_exists('bensemangat_kses_title')) {
 	/**
 	 * Sanitizes data for allowed HTML tags for post title.
 	 *
 	 * @param string $data Post title to filter.
 	 * @return string Filtered post title with allowed HTML tags and attributes intact.
 	 */
-	function bensemangat_kses_title( $data ) {
+	function bensemangat_kses_title($data)
+	{
 		// Tags not supported in HTML5 are not allowed.
 		$allowed_tags = array(
 			'abbr'             => array(),
@@ -232,8 +271,8 @@ if ( ! function_exists( 'bensemangat_kses_title' ) ) {
 			'u'                => array(),
 			'var'              => array(),
 		);
-		$allowed_tags = apply_filters( 'bensemangat_kses_title', $allowed_tags );
+		$allowed_tags = apply_filters('bensemangat_kses_title', $allowed_tags);
 
-		return wp_kses( $data, $allowed_tags );
+		return wp_kses($data, $allowed_tags);
 	}
 } // End of if function_exists( 'bensemangat_kses_title' ).
